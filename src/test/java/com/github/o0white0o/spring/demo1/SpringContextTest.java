9d0ba3568d01d01b85225eec4aa99e3f39834211
Alexey Drozdovskiy <aldrozdovskiy@gmail.com>
2022-11-10 21:04:47 +0300

    test(SpringDemo1): Optimize in 1 xml file

--- src/test/java/com/github/o0white0o/spring/demo1/SpringContextTest.java
+++ src/test/java/com/github/o0white0o/spring/demo1/SpringContextTest.java
@@ -3,6 +3,7 @@ package com.github.o0white0o.spring.demo1;
 import com.github.o0white0o.spring.demo1.service.IPersonService;
 import com.github.o0white0o.spring.demo1.service.PersonServiceImpl;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -10,13 +11,16 @@ public class SpringContextTest {
 
 private ClassPathXmlApplicationContext context;
 
+@BeforeEach
+public void setUp() {
+    context = new ClassPathXmlApplicationContext(
+            "spring/demo1/spring-context.xml");
+}
+
 @Test
 public void contextWithConstructorRefTest() {
 
-    context = new ClassPathXmlApplicationContext(
-            "spring/demo1/spring-context.xml");
-
-    IPersonService personService = context.getBean(PersonServiceImpl.class);
+    IPersonService personService = context.getBean("personService", PersonServiceImpl.class);
     Assertions.assertEquals("Person{name='Aleksei', age=26}",
                             personService.getByName("Aleksei").toString(),
                             "Error");
@@ -24,11 +28,7 @@ public void contextWithConstructorRefTest() {
 
 @Test
 public void contextWithPropertyRefTest() {
-
-    context = new ClassPathXmlApplicationContext(
-            "spring/demo1/spring-contextProp.xml");
-
-    IPersonService personService = context.getBean(PersonServiceImpl.class);
+    IPersonService personService = context.getBean("personServiceWithProp", PersonServiceImpl.class);
     Assertions.assertEquals("Person{name='Alex', age=27}",
                             personService.getByName("Alex").toString(),
                             "Error");
