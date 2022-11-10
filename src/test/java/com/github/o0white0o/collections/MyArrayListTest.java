bf70f9b855a58418584799f19a6ed23e372514ee
Alexey Drozdovskiy <aldrozdovskiy@gmail.com>
2022-11-10 11:22:05 +0300

    feat(MyArrayListTest): Update junit to 5 version

--- src/test/java/com/github/o0white0o/collections/MyArrayListTest.java
+++ src/test/java/com/github/o0white0o/collections/MyArrayListTest.java
@@ -1,8 +1,8 @@
 package com.github.o0white0o.collections;
 
-import org.junit.Before;
-import org.junit.Test;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
@@ -10,7 +10,7 @@ public class MyArrayListTest {
 
 private List<Integer> list;
 
-@Before
+@BeforeEach
 public void setUp() {
     list = new MyArrayList<>();
     list.add(1);
@@ -36,7 +36,6 @@ public void removeTest() {
 public void capacityTest() {
     int capacity = 1_000_000;
     list.clear();
-    Assertions.assertTrue(list.isEmpty(), "List is not empty");
     for (int i = 0; i < capacity; i++) {
         list.add(i);
     }
